edea1c3c6c2b3d92b2dc52dd4784004944757df4
Myrhoiazov <denis.mirgoyazov@gmail.com>
2023-04-19 00:43:06 +0200

    [DEPLOY on Host]

--- src/index.js
+++ src/index.js
@@ -3,49 +3,12 @@
     menuBtn: document.querySelector('[data-menu-button]'),
     mobileMenu: document.querySelector('[data-menu]'),
     arrModalOpeners: document.querySelectorAll('[data-btn-open]'),
-    backdrop: document.querySelector('[data-modal]'),
     arrBtnClose: document.querySelectorAll('.btn-close'),
     arrMenuItems: document.querySelectorAll('.nav__link'),
     btnGotoTop: document.querySelector('[data-goto-top]'),
   };
 
   if (refs.menuBtn) refs.menuBtn.addEventListener('click', toggleMenu);
-
-  if (refs.backdrop && refs.arrBtnClose && refs.arrModalOpeners) {
-    refs.arrModalOpeners.forEach(btn => {
-      btn.addEventListener('click', () => {
-        const modalClass = btn.getAttribute('data-btn-open');
-        if (modalClass.length == 0) return;
-        refs.backdrop.setAttribute('data-modal', modalClass);
-        const modalElement = document.querySelector(`.${modalClass}`);
-        if (modalElement)
-          modalElement.addEventListener('click', e => e.stopPropagation());
-        document.body.classList.add('no-scroll');
-
-        if (refs.menuBtn.classList.contains('is-open')) {
-          refs.menuBtn.classList.remove('is-open');
-          refs.menuBtn.setAttribute('aria-expanded', 'false');
-          refs.mobileMenu.classList.remove('is-open');
-          document.body.classList.remove('menu-opened');
-        }
-      });
-    });
-    refs.arrBtnClose.forEach(btn =>
-      btn.addEventListener('click', () => {
-        refs.backdrop.setAttribute('data-modal', '');
-        document.body.classList.remove('no-scroll');
-      })
-    );
-    refs.backdrop.addEventListener('click', () => {
-      refs.backdrop.setAttribute('data-modal', '');
-      document.body.classList.remove('no-scroll');
-    });
-  }
-
-  if (refs.arrMenuItems)
-    refs.arrMenuItems.forEach(item => {
-      item.addEventListener('click', toggleMenu);
-    });
 
   if (refs.btnGotoTop)
     refs.btnGotoTop.addEventListener('click', () => {
@@ -130,10 +93,6 @@ splide.mount();
 // Section animation
 const section = document.querySelectorAll('.js-hidden');
 const links = document.querySelectorAll('.nav__link');
-
-const options = {
-  threshold: 0.2,
-};
 
 const observer = new IntersectionObserver(addActiveClass);
 
