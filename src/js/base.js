9edb0cfe066108ddc921f77467ed06d4ec3d0ee7
Myrhoiazov <denis.mirgoyazov@gmail.com>
2024-01-14 22:37:10 +0100

    [Masters page] New styles.

--- src/js/base.js
+++ src/js/base.js
@@ -84,3 +84,32 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 });
+
+// Работа с картой подключенный код + локация
+
+// let map;
+
+// async function initMap() {
+//   // The location of Uluru
+//   const position = { lat: 48.461063189895135, lng: 35.04195727116427 };
+//   // Request needed libraries.
+//   //@ts-ignore
+//   const { Map } = await google.maps.importLibrary('maps');
+//   const { AdvancedMarkerView } = await google.maps.importLibrary('marker');
+
+//   // The map, centered at Uluru
+//   map = new Map(document.getElementById('map'), {
+//     zoom: 10,
+//     center: position,
+//     mapId: 'DEMO_MAP_ID',
+//   });
+
+//   //   The marker, positioned at Uluru
+//   const marker = new AdvancedMarkerView({
+//     map: map,
+//     position: position,
+//     title: 'Uluru',
+//   });
+// }
+
+// initMap();
